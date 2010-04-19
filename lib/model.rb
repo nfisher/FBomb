c4b050105e1dd129d4638b4d8e4fa63e9f313db2
Nathan Fisher <nfisher@junctionbox.ca>
2010-04-19 01:39:34 +0100

    Added determine finder and send to method.

--- lib/model.rb
+++ lib/model.rb
@@ -2,14 +2,17 @@
 # and open the template in the editor.
 
 module Facebook
+
   class Model
     attr_reader :table_name
+    attr_reader :facebook_connection
 
     # dynamic finder method, shamelessly extracted from rails
     def method_missing(method_id, *arguments)
       if match = method_id_to_matches(method_id.to_s)
         @table_name = match.captures.first
 
+        finder = determine_finder(match)
         attribute_names = []
         attribute_names ||= extract_attribute_names_from_match(match)
 
@@ -18,7 +21,9 @@ module Facebook
         case extra_options = arguments[attribute_names.size]
           when nil
             options = {:conditions => conditions}
+            send(finder, options)
           when Hash
+            send(finder)
         end
 
         return self
@@ -46,6 +51,5 @@ module Facebook
       }
       conditions.join(' AND ')
     end
-    
   end
 end
